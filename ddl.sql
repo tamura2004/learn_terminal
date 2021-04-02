345f1800d8a0f96568863b6abac2931b0db7f33f
M.Tamura <tamura2004@gmail.com>
2021-04-02 19:17:20 +0900

    Update data

--- ddl.sql
+++ ddl.sql
@@ -1,22 +1,23 @@
 create table country (
   country_id integer primary key autoincrement,
-  country_name varchar(20) not null unique
+  country_name varchar(255) not null unique
 );
 
 create table city (
   country_id integer not null,
   city_id integer not null,
-  city_name varchar(20) not null
+  city_name varchar(255) not null,
+  primary key(country_id, city_id)
 );
 
 create table customer (
   customer_id integer primary key,
-  customer_name varchar(20) not null
+  customer_name varchar(255) not null
 );
 
 create table attraction (
   attraction_id integer primary key,
-  attraction_name varchar(20) not null,
+  attraction_name varchar(255) not null,
   country_id integer,
   city_id integer,
   category_id integer
@@ -24,5 +25,5 @@ create table attraction (
 
 create table category (
   category_id primary key,
-  category_name varchar(20)
+  category_name varchar(255)
 );
